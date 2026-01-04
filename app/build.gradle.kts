7a774d5335d009d66fe503b9c6d11e0c73b91efb
Utkarsh Dalal <utkarsh.dalal@toptal.com>
2026-01-04 20:53:02 +0530

    bumped version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
         targetSdk = 28
 
         versionCode = 8
-        versionName = "0.6.1"
+        versionName = "0.6.2"
 
         buildConfigField("boolean", "GOLD", "false")
         fun secret(name: String) =
